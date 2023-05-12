75651f017ec9e8750e9c45ea0b7fb0e03c5a4368
Mahdi <amiralmasi021@gmail.com>
2023-05-12 12:55:03 +0330

    Design navbar when its not in the top and make it responsive

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import useMediaQuery from "./hooks/useMediaQuery";
 import Navbar from './components/Navbar/Navbar';
 import './App.css';
 const App = () => {
   const [selectedPage , setSelectedPage]  = useState('home');
+  const [isTopOfPage , setIsTopOfPage] = useState(true)
   const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)")
+  
+  useEffect(() =>{
+    const handelScroll= () =>{
+      if(window.scrollY === 0) setIsTopOfPage(true);
+      if( window.screenY !==0) setIsTopOfPage(false);
+    }
+    window.addEventListener("scroll" , handelScroll)
+    return () => window.removeEventListener("Scroll" , handelScroll)
+  },[])
+
   return (
     <div className="app bg-deep-blue">
-      <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage}/>
+      <Navbar isTopOfPage={isTopOfPage} selectedPage={selectedPage} setSelectedPage={setSelectedPage}/>
+      <div className='w-5/6 mx-auto md:h-full'>
+        {isAboveMediumScreens && ( <DotGroup 
+          selectedPage={selectedPage}
+          setSelectedPage={selectedPage}
+        />)}
+      </div>
     </div>
   );
 }
