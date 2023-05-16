71ff1679422a99c66cda5adbf090b21f4cd3bb06
Mahdi <amiralmasi021@gmail.com>
2023-05-16 14:00:10 +0330

    Add motion

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -14,16 +14,41 @@ const Landing = ({setSelectedPage}) => {
                 {isAboveMediumScreens ? (
                     <div className="relative z-0 ml-20 before:absolute before:-top-20 before:-left-20
                         before:rounded-t-[400px] before:w-full before:max-w-[400px] before:h-full before:border-2 before:border-blue before:z-[-1]">
-                        <img alt="profile" src={profileImage} />
+                        <img alt="profile" src={profileImage} className="hover:filter hover:saturate-200 transition duration-500 z-10 w-full max-w-[400] md:max-w-[600px] "/>
                     </div> 
                     ) 
                     : 
                     (<div>
-
+                        <img alt="profile" src={profileImage} className="hover:filter hover:saturate-200 transition duration-500 z-10 w-full max-w-[400] md:max-w-[600px] "/>
                     </div>)}
-
-            </div>
-
+                    </div>
+                    {/* Main section */}
+                    <div className="z-30 basis-2/5 mt-12 md:mt-32">
+                     {/* Headings */}
+                     <motion.div 
+                     initial="hidden"
+                     whileInView="visible" 
+                     viewport={{once : true , amount :0.5}} 
+                     transition={{duration : 0.5}}
+                     variants={{
+                        hidden:{opacity : 0 , x :-50 },
+                        visible : { opacity :1 , x: 0}
+                     }}
+                     >
+                        <p className="text-6xl font-playfair z-10 text-center md:text-start">
+                             Mahdi {""}
+                             <span className="xs:relative xs:text-deep-blue  xs:font-semibold z-20
+                                xs:before:content-brush before:absolute before:-left-[25px]
+                                before:-top-[70px] before:z-[-1]">
+                                Esper
+                             </span>
+                        </p>
+                        <p className="mt-10 mb-7 text-sm text-center md:text-start">
+                            ddddddddddddddddddddddddd
+                        </p>
+                     </motion.div>
+                    </div>
+            
         </section>    
     );
 }
